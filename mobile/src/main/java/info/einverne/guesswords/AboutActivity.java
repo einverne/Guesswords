beaeb7237c61ef48b42cece7220ce60639146066
Ein Verne <einverne@gmail.com>
2016-08-28 20:51:22 +0800

    Adjust about

--- mobile/src/main/java/info/einverne/guesswords/AboutActivity.java
+++ mobile/src/main/java/info/einverne/guesswords/AboutActivity.java
@@ -1,7 +1,8 @@
 package info.einverne.guesswords;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView versionNumber = (TextView) findViewById(R.id.versionNumber);
+        versionNumber.setText(BuildConfig.VERSION_NAME);
     }
 }
